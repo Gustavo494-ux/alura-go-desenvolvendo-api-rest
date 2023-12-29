73ed48c309e0a3b798b95829127e611974efadd5
Gustavo Gama <gustavogama494@gmail.com>
2023-12-28 21:05:22 -0300

    modulo 3 aula 1

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/Gustavo494-ux/go-api-rest/database"
 	"github.com/Gustavo494-ux/go-api-rest/models"
 	"github.com/Gustavo494-ux/go-api-rest/routes"
 )
@@ -12,6 +13,8 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
+
+	database.ConectaComBancoDeDados()
 
 	fmt.Println("Iniciando o servidor com go")
 	routes.HandleResquest()
